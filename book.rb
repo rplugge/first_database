629afe9017d5f0dc4046c2c43f62f60acd94e65e
Robert Plugge <robmplugge@hotmail.com>
2015-06-12 14:09:35 -0500

    Added Delete function to book.rb

--- book.rb
+++ book.rb
@@ -9,14 +9,15 @@ class Book
   end
   
   def self.add(name, genre, location, quantity)
-    CONNECTION.execute("INSERT INTO books (name, genre, location, quantity) VALUES ('#{name}', #{genre}, #{location}, #{quantity});")
+    CONNECTION.execute("INSERT INTO books (name, genre_id, location_id, quantity) VALUES ('#{name}', #{genre}, #{location}, #{quantity});")
   end
   
   def change_name(new_book_name)
-    CONNECTION.execute("UPDATE books SET name = '#{new_book_name}' WHERE book_id = #{@book_id};")
+    CONNECTION.execute("UPDATE books SET name = '#{new_book_name}' WHERE id = #{@book_id};")
   end
-
-  def
+  
+  def delete
+    CONNECTION.execute("DELETE FROM books WHERE id = #{@book_id};")
   end
   
 end
