14968c3c78cbd551183f082a75affd5e0e197af8
Robert Plugge <robmplugge@hotmail.com>
2015-06-18 14:28:02 -0500

    Updates 6-18. removed methods from classes, added functionality to module

--- genre.rb
+++ genre.rb
@@ -1,12 +1,38 @@
+require_relative "module.rb"
+require_relative "instance_module.rb"
+
 class Genre
   
-  def self.all
-    CONNECTION.execute("SELECT * FROM genres;")
+  extend ClassModule
+  include InstanceModule
+  
+  attr_reader :id
+  attr_accessor :name
+  
+  # - Initializes a new genre object. Takes its arguements in a hash. 
+  #
+  # - Example:
+  #       options = {"name" = "Fantasy"}
+  def initialize(options={})
+    @id = options["id"]
+    @name = options["name"]
+  end
+ 
+  # - Updates the assosiated row in the book table with the new values for a book object.
+  #
+  # - TODO - Needs to return something better
+  def save
+    CONNECTION.execute("UPDATE genres SET name = '#{@name}' WHERE id = #{id};")
   end
   
-  def self.add(genre)
-    CONNECTION.execute("INSERT INTO genres (genre) VALUES ('#{genre}');")
+  # - Checks to make sure the genre name is not nil or an empty string.
+  #
+  # - Returns T/F
+  def valid?
+    if @name == nil || @name == ""
+      false
+    else
+      true
+    end
   end
-  
-  
 end
